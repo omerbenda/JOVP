57c926c50889ecb95db8595fbff6abdc0af6a018
omerbenda <omerbenda@gmail.com>
2023-05-13 12:04:16 +0300

    feat(jovp.js): Made typesafe validation

--- jovp.test.js
+++ jovp.test.js
@@ -4,28 +4,24 @@ test('validateEqual - empty', () => {
     expect(jovp.validateEqual({}, {})).toBe(true);
 });
 
-test('validateEqual - same value', () => {
-    expect(jovp.validateEqual({a: 1}, {a: 1})).toBe(true);
+test('validateEqual - correct value', () => {
+    expect(jovp.validateEqual({a: 1}, {a: 'number'})).toBe(true);
 });
 
-test('validateEqual - different value', () => {
-    expect(jovp.validateEqual({b: 12}, {b: 4})).toBe(true);
-});
-
-test('validateEqual - different key same value', () => {
-    expect(jovp.validateEqual({a: 1}, {b: 1})).toBe(false);
-});
-
-test('validateEqual - different key different value', () => {
-    expect(jovp.validateEqual({a: 1}, {b: 2})).toBe(false);
+test('validateEqual - different key', () => {
+    expect(jovp.validateEqual({a: 1}, {b: 'number'})).toBe(false);
 });
 
 test('validateEqual - object empty', () => {
-    expect(jovp.validateEqual({}, {a: 1})).toBe(false);
+    expect(jovp.validateEqual({}, {a: 'string'})).toBe(false);
 });
 
 test('validateEqual - filter empty', () => {
     expect(jovp.validateEqual({a: 1}, {})).toBe(false);
+});
+
+test('validateEqual - different type', () => {
+    expect(jovp.validateEqual({a: 1}, {a: 'string'})).toBe(false);
 });
 
 test('validatefilter - empty', () => {
@@ -33,25 +29,25 @@ test('validatefilter - empty', () => {
 });
 
 test('validateFilter - same value', () => {
-    expect(jovp.validateFilter({a: 1}, {a: 1})).toBe(true);
+    expect(jovp.validateFilter({a: 1}, {a: 'number'})).toBe(true);
 });
 
 test('validateFilter - different value', () => {
-    expect(jovp.validateFilter({b: 12}, {b: 4})).toBe(true);
+    expect(jovp.validateFilter({b: 12}, {b: 'number'})).toBe(true);
 });
 
-test('validateFilter - different key same value', () => {
-    expect(jovp.validateFilter({a: 1}, {b: 1})).toBe(false);
-});
-
-test('validateFilter - different key different value', () => {
-    expect(jovp.validateFilter({a: 1}, {b: 2})).toBe(false);
+test('validateFilter - different key', () => {
+    expect(jovp.validateFilter({a: 1}, {b: 'number'})).toBe(false);
 });
 
 test('validateFilter - object empty', () => {
-    expect(jovp.validateFilter({}, {a: 1})).toBe(false);
+    expect(jovp.validateFilter({}, {a: 'number'})).toBe(false);
 });
 
 test('validateFilter - filter empty', () => {
     expect(jovp.validateFilter({a: 1}, {})).toBe(true);
+});
+
+test('validateFilter - different type', () => {
+    expect(jovp.validateFilter({a: 1}, {a: 'string'})).toBe(false);
 });
