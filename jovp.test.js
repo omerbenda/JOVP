42625949c6551f747a703e552bd0d0a85c21c234
omerbenda <omerbenda@gmail.com>
2023-05-13 17:00:12 +0300

    fix(jovp.test.js): Add destructuring to the test functions

--- jovp.test.js
+++ jovp.test.js
@@ -1,53 +1,53 @@
-const jovp = require('./jovp');
+const { validateEqual, validateMinimal } = require('./jovp.js');
 
 test('validateEqual - empty', () => {
-    expect(jovp.validateEqual({}, {})).toBe(true);
+    expect(validateEqual({}, {})).toBe(true);
 });
 
 test('validateEqual - correct value', () => {
-    expect(jovp.validateEqual({a: 1}, {a: 'number'})).toBe(true);
+    expect(validateEqual({a: 1}, {a: 'number'})).toBe(true);
 });
 
 test('validateEqual - different key', () => {
-    expect(jovp.validateEqual({a: 1}, {b: 'number'})).toBe(false);
+    expect(validateEqual({a: 1}, {b: 'number'})).toBe(false);
 });
 
 test('validateEqual - object empty', () => {
-    expect(jovp.validateEqual({}, {a: 'string'})).toBe(false);
+    expect(validateEqual({}, {a: 'string'})).toBe(false);
 });
 
 test('validateEqual - filter empty', () => {
-    expect(jovp.validateEqual({a: 1}, {})).toBe(false);
+    expect(validateEqual({a: 1}, {})).toBe(false);
 });
 
 test('validateEqual - different type', () => {
-    expect(jovp.validateEqual({a: 1}, {a: 'string'})).toBe(false);
+    expect(validateEqual({a: 1}, {a: 'string'})).toBe(false);
 });
 
 test('validatefilter - empty', () => {
-    expect(jovp.validateMinimal({}, {})).toBe(true);
+    expect(validateMinimal({}, {})).toBe(true);
 });
 
 test('validateFilter - same value', () => {
-    expect(jovp.validateMinimal({a: 1}, {a: 'number'})).toBe(true);
+    expect(validateMinimal({a: 1}, {a: 'number'})).toBe(true);
 });
 
 test('validateFilter - different value', () => {
-    expect(jovp.validateMinimal({b: 12}, {b: 'number'})).toBe(true);
+    expect(validateMinimal({b: 12}, {b: 'number'})).toBe(true);
 });
 
 test('validateFilter - different key', () => {
-    expect(jovp.validateMinimal({a: 1}, {b: 'number'})).toBe(false);
+    expect(validateMinimal({a: 1}, {b: 'number'})).toBe(false);
 });
 
 test('validateFilter - object empty', () => {
-    expect(jovp.validateMinimal({}, {a: 'number'})).toBe(false);
+    expect(validateMinimal({}, {a: 'number'})).toBe(false);
 });
 
 test('validateFilter - filter empty', () => {
-    expect(jovp.validateMinimal({a: 1}, {})).toBe(true);
+    expect(validateMinimal({a: 1}, {})).toBe(true);
 });
 
 test('validateFilter - different type', () => {
-    expect(jovp.validateMinimal({a: 1}, {a: 'string'})).toBe(false);
+    expect(validateMinimal({a: 1}, {a: 'string'})).toBe(false);
 });
